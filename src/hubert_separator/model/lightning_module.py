f063e9ae642f9913fd2826ca4088939fa0e824c1
Kohei ASAI <ko2001he@gmail.com>
2025-04-01 15:59:36 +0900

    add hubert model

--- src/hubert_separator/model/lightning_module.py
+++ src/hubert_separator/model/lightning_module.py
@@ -2,12 +2,18 @@ import hydra
 import torch
 from lightning.pytorch import LightningModule
 from omegaconf import DictConfig
+from transformers import HubertModel
 
 
 class HuBERTSeparatorLightningModule(LightningModule):
     def __init__(self, cfg: DictConfig) -> None:
         super(HuBERTSeparatorLightningModule, self).__init__()
         self.cfg = cfg
+
+        self.hubert_model = HubertModel.from_pretrained(
+            cfg.model.hubert.model_name
+        ).train(True)
+
         self.save_hyperparameters(cfg)
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
