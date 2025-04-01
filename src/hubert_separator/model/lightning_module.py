0b0a8d231d5c4237b75f57b7f6fb5fee4c2ed6c1
Kohei ASAI <ko2001he@gmail.com>
2025-04-01 15:55:53 +0900

    add optimizer

--- src/hubert_separator/model/lightning_module.py
+++ src/hubert_separator/model/lightning_module.py
@@ -1,3 +1,5 @@
+import hydra
+import torch
 from lightning.pytorch import LightningModule
 from omegaconf import DictConfig
 
@@ -6,3 +8,9 @@ class HuBERTSeparatorLightningModule(LightningModule):
     def __init__(self, cfg: DictConfig) -> None:
         super(HuBERTSeparatorLightningModule, self).__init__()
         self.cfg = cfg
+        self.save_hyperparameters(cfg)
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        return hydra.utils.instantiate(
+            self.cfg.model.optimizer, params=self.parameters()
+        )
