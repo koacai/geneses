7bc94a49b602152ecac43a8740147244f786e2ba
Kohei ASAI <ko2001he@gmail.com>
2025-04-01 16:06:19 +0900

    add calc loss

--- src/hubert_separator/model/lightning_module.py
+++ src/hubert_separator/model/lightning_module.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 import hydra
 import torch
 from lightning.pytorch import LightningModule
+from lightning.pytorch.utilities.types import STEP_OUTPUT
 from omegaconf import DictConfig
 from transformers import HubertModel
 
@@ -20,3 +23,24 @@ class HuBERTSeparatorLightningModule(LightningModule):
         return hydra.utils.instantiate(
             self.cfg.model.optimizer, params=self.parameters()
         )
+
+    def training_step(self, batch: dict[str, Any], batch_idx: int) -> STEP_OUTPUT:
+        _ = batch_idx
+        loss = self.calc_loss(batch)
+
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch: dict[str, Any], batch_idx: int) -> STEP_OUTPUT:
+        _ = batch_idx
+        loss = self.calc_loss(batch)
+
+        self.log("validation_loss", loss)
+        return loss
+
+    def calc_loss(self, batch: dict[str, Any]) -> torch.Tensor:
+        src = self.hubert_model(
+            **batch["ssl_input_merged"], output_hidden_states=True
+        ).hidden_states[self.cfg.model.hubert.layer]
+
+        return src
--- tests/model/test_lightning_module.py
+++ tests/model/test_lightning_module.py
@@ -1,6 +1,10 @@
+import itertools
+
 import pytest
+import torch
 from hydra import compose, initialize
 
+from hubert_separator.data.datamodule import HuBERTSeparatorDataModule
 from hubert_separator.model.lightning_module import HuBERTSeparatorLightningModule
 
 
@@ -10,6 +14,14 @@ class TestHuBERTSeparatorLightningModule:
         with initialize(config_path="../../config", version_base=None):
             cfg = compose(config_name="default")
             self.lightning_module = HuBERTSeparatorLightningModule(cfg)
+            self.datamodule = HuBERTSeparatorDataModule(cfg.data.datamodule)
+            self.datamodule.setup("fit")
 
     def test_init(self, init) -> None:
         _ = init
+
+    def test_calc_loss(self, init) -> None:
+        _ = init
+        for batch in itertools.islice(self.datamodule.val_dataloader(), 3):
+            loss = self.lightning_module.calc_loss(batch)
+            assert isinstance(loss, torch.Tensor)
