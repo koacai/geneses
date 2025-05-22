01488e58737b0fe81d780e563721236a8029f5d7
Kohei ASAI <ko2001he@gmail.com>
2025-05-22 12:25:33 +0900

    add test hifigan

--- tests/hifigan/test_utils.py
+++ tests/hifigan/test_utils.py
@@ -8,5 +8,4 @@ from dialogue_separator.hifigan.utils import get_vocoder
 def test_get_vocoder() -> None:
     path = Path("LJ_FT_T2_V1/generator_v1")
     device = torch.device("cpu")
-    hifigan = get_vocoder(path, device)
-    print(hifigan)
+    _ = get_vocoder(path, device)
