e199b9046ca09430db0abc77326e90d330d1ad32
Kohei ASAI <ko2001he@gmail.com>
2025-07-08 11:45:22 +0900

    add create mask func

---
+++ src/dialogue_separator/util/util.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def create_mask(lengths: torch.Tensor, feature: torch.Tensor) -> torch.Tensor:
+    assert lengths.dim() == 1
+    assert lengths.shape[0] == feature.shape[0]
+
+    mask = torch.zeros_like(feature, device=feature.device, dtype=torch.bool)
+
+    for i, length in enumerate(lengths):
+        if length > 0:
+            mask[i, :length] = True
+        else:
+            mask[i, :] = False
+
+    return mask
