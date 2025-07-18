7292ff39f9ec85519216f637ce4af4258ec4afe7
Kohei ASAI <ko2001he@gmail.com>
2025-07-18 17:35:31 +0900

    calc utmos

--- src/dialogue_separator/metrics/nonintrusive_se/utmos.py
+++ src/dialogue_separator/metrics/nonintrusive_se/utmos.py
@@ -1,7 +1,12 @@
 import torch
 
 
-def calc_utmos() -> None:
+def calc_utmos(audio: torch.Tensor, sr: int, use_gpu: bool) -> float:
     utmos_tag = "utmos22_strong"
     model = torch.hub.load("tarepan/SpeechMOS:v1.2.0", utmos_tag, trust_repo=True)
-    print(model)
+
+    device = torch.device("cuda" if use_gpu else "cpu")
+    model.device = device  # type: ignore
+
+    score = model(audio.unsqueeze(0), sr)  # type: ignore
+    return score.cpu().item()
--- tests/metrics/nonintrusive_se/test_utmos.py
+++ tests/metrics/nonintrusive_se/test_utmos.py
@@ -1,5 +1,9 @@
+import torch
+
 from dialogue_separator.metrics.nonintrusive_se.utmos import calc_utmos
 
 
 def test_calc_utmos() -> None:
-    calc_utmos()
+    audio = torch.randn(16000 * 10)
+    score = calc_utmos(audio, 16000, True)
+    assert isinstance(score, float)
