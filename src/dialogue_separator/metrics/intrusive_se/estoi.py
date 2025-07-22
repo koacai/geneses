560d29b9acee3e3f969262bb4b46ca68b61bdb40
Kohei ASAI <ko2001he@gmail.com>
2025-07-22 13:02:21 +0900

    cleanup

--- src/dialogue_separator/metrics/intrusive_se/estoi.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-from pystoi import stoi
-
-
-def calc_estoi(ref: torch.Tensor, inf: torch.Tensor, sr: int) -> float:
-    np.random.seed(0)
-    return stoi(ref.cpu().numpy(), inf.cpu().numpy(), fs_sig=sr, extended=True)
