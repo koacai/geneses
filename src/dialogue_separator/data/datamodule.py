fb3fa73b118da9a6814b07ccbc593946f83cebbe
Kohei ASAI <ko2001he@gmail.com>
2025-07-30 16:12:02 +0900

    fix datamodule

--- src/dialogue_separator/data/datamodule.py
+++ src/dialogue_separator/data/datamodule.py
@@ -1,27 +1,25 @@
 from typing import Any
 
 import torch
-import torch.nn.functional as F
-import torchaudio
 import webdataset as wds
 from lightning.pytorch import LightningDataModule
 from omegaconf import DictConfig
 from torch.nn.utils.rnn import pad_sequence
-from transformers import AutoFeatureExtractor
+
+from dialogue_separator.data.util import glob_wds
 
 
 class DialogueSeparatorDataModule(LightningDataModule):
     def __init__(self, cfg: DictConfig) -> None:
         super(DialogueSeparatorDataModule, self).__init__()
         self.cfg = cfg
-        self.processor = AutoFeatureExtractor.from_pretrained(cfg.ssl_model.name)
 
     def setup(self, stage: str) -> None:
         nodesplitter = wds.split_by_worker if self.cfg.use_ddp else wds.single_node_only
         if stage == "fit":
             self.train_dataset = (
                 wds.WebDataset(
-                    self.cfg.train.dataset_path,
+                    glob_wds(self.cfg.train.dataset_dir),
                     shardshuffle=100,
                     nodesplitter=nodesplitter,
                     repeat=True,
@@ -32,7 +30,7 @@ class DialogueSeparatorDataModule(LightningDataModule):
 
             self.valid_dataset = (
                 wds.WebDataset(
-                    self.cfg.valid.dataset_path,
+                    glob_wds(self.cfg.valid.dataset_dir),
                     shardshuffle=False,
                     nodesplitter=nodesplitter,
                     repeat=True,
@@ -44,7 +42,7 @@ class DialogueSeparatorDataModule(LightningDataModule):
         elif stage == "test":
             self.test_dataset = (
                 wds.WebDataset(
-                    self.cfg.test.dataset_path,
+                    glob_wds(self.cfg.test.dataset_dir),
                     shardshuffle=False,
                     nodesplitter=wds.single_node_only,
                     repeat=True,
@@ -90,76 +88,51 @@ class DialogueSeparatorDataModule(LightningDataModule):
         return x[0]
 
     def collate_fn(self, batch) -> dict[str, Any]:
-        max_duration = self.cfg.vae.max_duration
-
-        wav_1 = torch.zeros(len(batch), self.cfg.vae.sample_rate * max_duration)
-        wav_2 = torch.zeros(len(batch), self.cfg.vae.sample_rate * max_duration)
-        wav_merged = torch.zeros(len(batch), self.cfg.vae.sample_rate * max_duration)
-
+        raw_wav_1 = []
+        raw_wav_2 = []
+        clean_wav = []
+        noisy_wav = []
         wav_len = []
+        vae_len = []
         vae_feature_1 = []
         vae_feature_2 = []
-        vae_len = []
         text_1 = []
         text_2 = []
-        wav_ssl_input = []
+        input_features = []
+        attention_mask = []
 
-        for i, sample in enumerate(batch):
-            dialogue, sr = sample["audio.flac"]
+        for sample in batch:
+            raw_wav_1.append(sample["raw_wav_1.pth"].squeeze())
+            raw_wav_2.append(sample["raw_wav_2.pth"].squeeze())
+            clean_wav.append(sample["clean_wav.pth"].squeeze())
+            noisy_wav.append(sample["noisy_wav.pth"].squeeze())
+            wav_len.append(sample["wav_len.pth"][0])
+            vae_len.append(sample["vae_len.pth"][0])
+            vae_feature_1.append(sample["vae_feature_1.pth"].squeeze())
+            vae_feature_2.append(sample["vae_feature_2.pth"].squeeze())
+            text_1.append(sample["text_1.pickle"][0])
+            text_2.append(sample["text_2.pickle"][0])
 
-            if sr != self.cfg.vae.sample_rate:
-                dialogue = torchaudio.functional.resample(
-                    dialogue, sr, self.cfg.vae.sample_rate
-                )
+            ssl_input = sample["ssl_input.pickle"]
+            input_features.append(ssl_input["input_features"].squeeze())
+            attention_mask.append(ssl_input["attention_mask"].squeeze())
 
-            dialogue = dialogue[:, : self.cfg.vae.sample_rate * max_duration]
-            wav_1[i, : dialogue.shape[-1]] = dialogue[0]
-            wav_2[i, : dialogue.shape[-1]] = dialogue[1]
-            wav_merged[i, : dialogue.shape[-1]] = dialogue[0] + dialogue[1]
+        input_features_padded = pad_sequence(input_features, batch_first=True)
+        attention_mask_padded = pad_sequence(attention_mask, batch_first=True)
 
-            _wav_len = dialogue.shape[-1]
-            wav_len.append(_wav_len)
-
-            vae_feature_1.append(sample["vae_feature_1.pth"])
-            vae_feature_2.append(sample["vae_feature_2.pth"])
-
-            _vae_len = (
-                sample["vae_feature_1.pth"].shape[-1] * _wav_len // wav_1.shape[-1]
-            )
-            vae_len.append(_vae_len)
-
-            text_1.append(sample["text_1.txt"])
-            text_2.append(sample["text_2.txt"])
-
-            if sr != self.cfg.ssl_model.sample_rate:
-                dialogue = torchaudio.functional.resample(
-                    dialogue, sr, self.cfg.ssl_model.sample_rate
-                )
-
-            _wav_ssl_input = dialogue[0] + dialogue[1]
-            _wav_ssl_input = F.pad(_wav_ssl_input, (40, 40), mode="constant", value=0)
-            wav_ssl_input.append(_wav_ssl_input)
-
-        vae_feature_1 = pad_sequence(vae_feature_1, batch_first=True)
-        vae_feature_2 = pad_sequence(vae_feature_2, batch_first=True)
-
-        ssl_input = self.processor(
-            [w.cpu().numpy() for w in wav_ssl_input],
-            sampling_rate=self.cfg.ssl_model.sample_rate,
-            return_tensors="pt",
-        )
-
-        output = {
-            "wav_1": wav_1,
-            "wav_2": wav_2,
-            "wav_merged": wav_merged,
-            "wav_len": torch.tensor(wav_len),
-            "vae_len": torch.tensor(vae_len),
-            "vae_feature_1": vae_feature_1,
-            "vae_feature_2": vae_feature_2,
-            "ssl_input": ssl_input,
+        return {
+            "raw_wav_1": torch.stack(raw_wav_1),
+            "raw_wav_2": torch.stack(raw_wav_2),
+            "clean_wav": torch.stack(clean_wav),
+            "noisy_wav": torch.stack(noisy_wav),
+            "wav_len": torch.stack(wav_len),
+            "vae_len": torch.stack(vae_len),
+            "vae_feature_1": torch.stack(vae_feature_1),
+            "vae_feature_2": torch.stack(vae_feature_2),
             "text_1": text_1,
             "text_2": text_2,
+            "ssl_input": {
+                "input_features": input_features_padded,
+                "attention_mask": attention_mask_padded,
+            },
         }
-
-        return output
--- tests/data/test_datamodule.py
+++ tests/data/test_datamodule.py
@@ -1,62 +1,25 @@
 import itertools
 
-import pytest
 from hydra import compose, initialize
 
 from dialogue_separator.data.datamodule import DialogueSeparatorDataModule
 
 
-class TestDialogueSeparatorDataModule:
-    @pytest.fixture
-    def init(self) -> None:
-        with initialize(config_path="../../config", version_base=None):
-            cfg = compose(config_name="default").data.datamodule
-            self.datamodule = DialogueSeparatorDataModule(cfg)
+def test_augment_datamodule_test_dataloader() -> None:
+    with initialize(config_path="../../config", version_base=None):
+        cfg = compose(config_name="default").data.datamodule
+        datamodule = DialogueSeparatorDataModule(cfg)
 
-    def test_train_dataloader(self, init) -> None:
-        _ = init
-        self.datamodule.setup("fit")
-
-        batch_size = self.datamodule.cfg.train.batch_size
-
-        for batch in itertools.islice(self.datamodule.train_dataloader(), 3):
-            assert batch["wav_1"].size(0) == batch_size
-            assert batch["wav_2"].size(0) == batch_size
-            assert batch["wav_merged"].size(0) == batch_size
-            assert batch["wav_len"].size(0) == batch_size
-            assert batch["vae_len"].size(0) == batch_size
-            assert batch["vae_feature_1"].size(0) == batch_size
-            assert batch["vae_feature_2"].size(0) == batch_size
-            assert isinstance(batch["ssl_input"], dict)
-
-    def test_val_dataloader(self, init) -> None:
-        _ = init
-        self.datamodule.setup("fit")
-
-        batch_size = self.datamodule.cfg.valid.batch_size
-
-        for batch in itertools.islice(self.datamodule.val_dataloader(), 3):
-            assert batch["wav_1"].size(0) == batch_size
-            assert batch["wav_2"].size(0) == batch_size
-            assert batch["wav_merged"].size(0) == batch_size
-            assert batch["wav_len"].size(0) == batch_size
-            assert batch["vae_len"].size(0) == batch_size
-            assert batch["vae_feature_1"].size(0) == batch_size
-            assert batch["vae_feature_2"].size(0) == batch_size
-            assert isinstance(batch["ssl_input"], dict)
-
-    def test_test_dataloader(self, init) -> None:
-        _ = init
-        self.datamodule.setup("test")
-
-        batch_size = self.datamodule.cfg.test.batch_size
-
-        for batch in itertools.islice(self.datamodule.test_dataloader(), 3):
-            assert batch["wav_1"].size(0) == batch_size
-            assert batch["wav_2"].size(0) == batch_size
-            assert batch["wav_merged"].size(0) == batch_size
-            assert batch["wav_len"].size(0) == batch_size
-            assert batch["vae_len"].size(0) == batch_size
-            assert batch["vae_feature_1"].size(0) == batch_size
-            assert batch["vae_feature_2"].size(0) == batch_size
-            assert isinstance(batch["ssl_input"], dict)
+    datamodule.setup("test")
+    batch_size = datamodule.cfg.test.batch_size
+    for batch in itertools.islice(datamodule.test_dataloader(), 3):
+        assert batch["raw_wav_1"].size(0) == batch_size
+        assert batch["raw_wav_2"].size(0) == batch_size
+        assert batch["clean_wav"].size(0) == batch_size
+        assert batch["wav_len"].size(0) == batch_size
+        assert batch["vae_len"].size(0) == batch_size
+        assert batch["vae_feature_1"].size(0) == batch_size
+        assert batch["vae_feature_2"].size(0) == batch_size
+        assert len(batch["text_1"]) == batch_size
+        assert len(batch["text_2"]) == batch_size
+        assert "ssl_input" in batch
