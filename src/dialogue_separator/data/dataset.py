8f5c5e73220f06f028f974d03bc3c93d77324af6
Kohei ASAI <ko2001he@gmail.com>
2025-08-01 13:55:43 +0900

    init dataset

--- src/dialogue_separator/data/dataset.py
+++ src/dialogue_separator/data/dataset.py
@@ -2,12 +2,13 @@ from typing import Any, Generator
 
 import torch
 from lhotse import CutSet
-from webdataset import filters
+from webdataset import FluidInterface
 from webdataset.pipeline import DataPipeline
 
 
-class LibriTTSRMixDataset(DataPipeline):
+class LibriTTSRMixDataset(DataPipeline, FluidInterface):
     def __init__(self, cuts: CutSet) -> None:
+        super(LibriTTSRMixDataset, self).__init__()
         self.cuts = cuts
 
     def __iter__(self) -> Generator[dict[str, Any], None, None]:
@@ -24,6 +25,3 @@ class LibriTTSRMixDataset(DataPipeline):
                 "wav_len_1": cut.supervisions[0].custom["wav_len"],
                 "wav_len_2": cut.supervisions[1].custom["wav_len"],
             }
-
-    def map(self, f, handler=filters.reraise_exception):
-        return self.compose(filters.map(f, handler=handler))
--- src/dialogue_separator/data/preprocess_datamodule.py
+++ src/dialogue_separator/data/preprocess_datamodule.py
@@ -37,30 +37,41 @@ class PreprocessDataModule(LightningDataModule):
         test_cuts = cuts.filter(lambda c: _in_subset(c, ["test-clean"]))
 
         self.train_dataset = self.setup_dataset_pipeline(
-            LibriTTSRMixDataset(train_cuts)
+            LibriTTSRMixDataset(train_cuts), self.cfg.batch_size
         )
         self.valid_dataset = self.setup_dataset_pipeline(
-            LibriTTSRMixDataset(valid_cuts)
+            LibriTTSRMixDataset(valid_cuts), self.cfg.batch_size
         )
-        self.test_dataset = self.setup_dataset_pipeline(LibriTTSRMixDataset(test_cuts))
+        self.test_dataset = self.setup_dataset_pipeline(
+            LibriTTSRMixDataset(test_cuts), self.cfg.batch_size
+        )
 
     def setup_dataset_pipeline(
-        self, dataset: LibriTTSRMixDataset
+        self, dataset: LibriTTSRMixDataset, batch_size: int
     ) -> LibriTTSRMixDataset:
+        dataset = self.init_dataset(dataset).batched(
+            batch_size, collation_fn=self.collate_fn
+        )
         return dataset
 
     def init_dataset(self, dataset: LibriTTSRMixDataset) -> LibriTTSRMixDataset:
-        dataset = dataset.map(partial(self.lowcut, input_key="audio", cutoff=50))
+        dataset = (
+            dataset.map(partial(self.lowcut, input_key="audio", cutoff=50))
+            .map(
+                partial(self.rename_audio, input_key="audio", output_key="clean_stereo")
+            )
+            .map(partial(self.stereo_to_mono, input_key="audio", output_key="noisy"))
+            .map(partial(self.stereo_to_mono, input_key="audio", output_key="clean"))
+        )
         return dataset
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
             self.train_dataset,
-            batch_size=self.cfg.batch_size,
             num_workers=self.cfg.num_workers,
             pin_memory=True,
             shuffle=False,
-            collate_fn=self.collate_fn,
+            collate_fn=self.identity,
             drop_last=True,
             persistent_workers=True,
         )
@@ -68,11 +79,10 @@ class PreprocessDataModule(LightningDataModule):
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
             self.valid_dataset,
-            batch_size=self.cfg.batch_size,
             num_workers=self.cfg.num_workers,
             pin_memory=True,
             shuffle=False,
-            collate_fn=self.collate_fn,
+            collate_fn=self.identity,
             drop_last=True,
             persistent_workers=True,
         )
@@ -80,11 +90,10 @@ class PreprocessDataModule(LightningDataModule):
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
             self.test_dataset,
-            batch_size=self.cfg.batch_size,
             num_workers=self.cfg.num_workers,
             pin_memory=True,
             shuffle=False,
-            collate_fn=self.collate_fn,
+            collate_fn=self.identity,
             drop_last=True,
             persistent_workers=True,
         )
@@ -97,6 +106,9 @@ class PreprocessDataModule(LightningDataModule):
             text_2.append(sample["text_2"])
         return {"text_1": text_1, "text_2": text_2}
 
+    def identity(self, x):
+        return x[0]
+
     @staticmethod
     @torch.inference_mode()
     def lowcut(sample, input_key: str, cutoff=50):
@@ -105,3 +117,19 @@ class PreprocessDataModule(LightningDataModule):
         new_sample = sample.copy()
         new_sample[input_key] = (wav, sr)
         return new_sample
+
+    @staticmethod
+    def rename_audio(sample, output_key: str, input_key: str | None = None):
+        if input_key is None:
+            audio_key = [k for k in sample.keys() if "audio" in k][0]
+        else:
+            audio_key = input_key
+        sample[output_key] = sample[audio_key]
+        return sample
+
+    @staticmethod
+    def stereo_to_mono(sample, output_key: str, input_key: str):
+        wav, sr = sample[input_key]
+        assert wav.shape[0] == 2
+        sample[output_key] = (wav[0] + wav[1], sr)
+        return sample
