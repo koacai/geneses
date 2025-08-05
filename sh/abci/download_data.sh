be41086e14f3371516d194cfe07336272183f0c8
Kohei ASAI <ko2001he@gmail.com>
2025-08-05 10:02:04 +0900

    download data

---
+++ sh/abci/download_data.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#PBS -q rt_HC
+#PBS -l select=1
+#PBS -P gag51394
+#PBS -l walltime=12:00:00
+#PBS -j oe
+#PBS -k oed
+
+cd ${PBS_O_WORKDIR}
+PYTHONUTF8=1 uv run scripts/download_data.py
