ef177461037f704fefc13004033bcb1928348af4
Kohei ASAI <ko2001he@gmail.com>
2025-09-30 14:21:23 +0900

    huggingface

--- scripts/upload_to_huggingface.py
+++ scripts/upload_to_huggingface.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from huggingface_hub import HfApi
 
 if __name__ == "__main__":
-    ckpt_path = "dialogue-separator/rzjrr29f/checkpoints/epoch=112-step=57404.ckpt"
-    config_path = "outputs/2025-06-23/14-18-00/.hydra/config.yaml"
-    metadata_path = "wandb/run-20250623_141828-rzjrr29f/files/wandb-metadata.json"
+    ckpt_path = "flowditse/6df5kvvo/checkpoints/epoch=20-step=151515.ckpt"
+    config_path = "outputs/2025-09-24/10-52-37/.hydra/config.yaml"
+    metadata_path = "wandb/run-20250924_105313-6df5kvvo/files/wandb-metadata.json"
 
     keys_to_extract_from_metadata = [
         "os",
@@ -35,25 +35,25 @@ if __name__ == "__main__":
     with open(tmp_metadata_path, "w") as f:
         json.dump(metadata_filtered, f, indent=2)
 
-    dir = "Libri2Mix/L1Loss"
+    dir = "complex_noise"
 
     api = HfApi()
     api.upload_file(
         path_or_fileobj=ckpt_path,
         path_in_repo=f"{dir}/{Path(ckpt_path).name}",
-        repo_id="koacai/dialogue-separator",
+        repo_id="koacai/flowditse",
         repo_type="model",
     )
     api.upload_file(
         path_or_fileobj=config_path,
         path_in_repo=f"{dir}/config.yaml",
-        repo_id="koacai/dialogue-separator",
+        repo_id="koacai/flowditse",
         repo_type="model",
     )
     api.upload_file(
         path_or_fileobj=tmp_metadata_path,
         path_in_repo=f"{dir}/metadata.json",
-        repo_id="koacai/dialogue-separator",
+        repo_id="koacai/flowditse",
         repo_type="model",
     )
 
